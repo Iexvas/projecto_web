a4f9d15cf7694ecfc8d42659ce8c80611cf167d7
Ismael Freire <brandon.freire@outlook.com>
2026-05-30 11:10:42 -0500

    Agregar el boton de mute

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -14,11 +14,14 @@ export class Level1 extends Phaser.Scene {
     }
 
     preload() {
-        // Cargar el fondo desde la carpeta
+        // ----------------Cargar el fondo desde la carpeta----------------
         this.load.image('fondo_selva', '/src/assets/levels/selva.png');
 
-        // Cargar el sonido de fondo para el nivel
+        // ----------------Cargar el sonido de fondo para el nivel-----------
         this.load.audio('nivel1_music', '/src/assets/audio/theme_level1.mp3');
+
+        // ----------------Cargar boton de mute-----------------------
+        this.load.image('mute_button', '/src/assets/ui/mute.png');
 
         // Marcador de posicion temporal para el jugador
         const canvas = document.createElement('canvas');
@@ -36,7 +39,7 @@ export class Level1 extends Phaser.Scene {
         // Renderizar el fondo de la selva
         this.background = this.add.image(800, 360, 'fondo_selva');
 
-        // Reproducir música de fondo para el nivel
+        // ----------Reproducir música de fondo para el nivel----------------
         this.musicaNivel1 = this.sound.add('nivel1_music', {
             loop: true,
             volume: 0.5
@@ -44,6 +47,7 @@ export class Level1 extends Phaser.Scene {
         // Iniciar la reproducción de la música del nivel
         this.musicaNivel1.play();
 
+        //----------Crear el jugador y las plataformas----------------
         // Instanciamos a nuestro jugador en el centro superior de la pantalla
         this.player = new Player(this, 100, 400);
 
@@ -55,11 +59,34 @@ export class Level1 extends Phaser.Scene {
         ground.setAlpha(0.6); // Opcional: hazlo semitransparente para ver el fondo debajo
         this.platforms.add(ground);
 
-        // REQUISITO OBLIGATORIO: Detección de colisiones 
         // Hacemos que el jugador colisione con el suelo
         this.physics.add.collider(this.player, this.platforms);
 
-        // Pausa del juego
+        // ------------Botón de mute para la música------------
+        // Iniciar el juego con sonido
+        this.sound.mute = false;
+
+        // Crear el botón de mute en la esquina superior derecha
+        this.btnMute = this.add.image(750, 50, 'mute_button').setInteractive();
+        // Mantener el botón fijo en la pantalla aunque el jugador se mueva
+        this.btnMute.setScrollFactor(0);
+        // Ajustar escala del boton
+        this.btnMute.setScale(0.05);
+        // Al inicio el boton es transparente para indicar que el sonido está activo
+        this.btnMute.setAlpha(0.5);
+        // Interracion con el boton
+        this.btnMute.on('pointerdown', () => {
+            // Invertir el estado global del sonido en todo el juego
+            this.sound.mute = !this.sound.mute;
+            
+            // Cambiar la transparencia de la imagen para el usuario
+            this.btnMute.setAlpha(this.sound.mute ? 0.5 : 1.0);
+
+            // Guardar la nueva preferencia en el navegador
+            localStorage.setItem('isMuted', this.sound.mute);
+        });
+
+        // ----------------Pausa del juego----------------------
         this.input.keyboard.on('keydown-ESC', () => {
             // Pausamos la escena actual
             this.scene.pause();
